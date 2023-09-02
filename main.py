db0d8c499ee6c1d05655aea991fa2f01a6e6408e
aminaibrahimelhendy <amina24092007@gmail.com>
2023-09-02 20:04:23 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+def my_name(name):
+    print("my name is" , name)
+my_name("Amina")
+
+def my_meal(food,drink):
+    print("I like to eat",food, "and while drinking",drink)
+my_meal("pizza","soda")
+
+def cube(number):
+    return number**3
+def by_three(number):
+    if number%3==0:
+      return cube 
+    else:
+        return("False")
+print(by_three(6))
+    
